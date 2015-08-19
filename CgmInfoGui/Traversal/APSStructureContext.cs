9fe69d146cf13bc8dbd5bbfca7ab1a24e588f34e
BhaaL <bhaal@0x1337.org>
2015-08-19 20:55:28 +0200

    parse WebCGM 2.0 APS Region

--- CgmInfoGui/Traversal/APSStructureContext.cs
+++ CgmInfoGui/Traversal/APSStructureContext.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using CgmInfo.Commands.ApplicationStructureDescriptor;
 using CgmInfoGui.ViewModels.Nodes;
@@ -44,6 +46,13 @@ namespace CgmInfoGui.Traversal
                         allValues[0], allValues[1], allValues[2]);
                     break;
 
+                case "REGION": // [WebCGM20-IC 3.2.2.1]
+                    if (allValues.Length < 2)
+                        goto default;
+
+                    WebCGM20Region(applicationStructureAttribute, allValues);
+                    break;
+
                 #endregion
 
                 default:
@@ -57,6 +66,137 @@ namespace CgmInfoGui.Traversal
                         var attributeNode = AddNode("@{0} [{1} values]", applicationStructureAttribute.AttributeType, allValues.Length);
                         attributeNode.Nodes.AddRange(allValues.Select(value => new SimpleNode(Convert.ToString(value))));
                     }
+                    break;
+            }
+        }
+
+        private void WebCGM20Region(ApplicationStructureAttribute applicationStructureAttribute, object[] allValues)
+        {
+            // The data record is an SDR of one or more member pairs (i.e., 2*m members, m>=1).
+            // Each member-pair defines a simple region: the first member is of data type Index, whose valid values are:
+            //      1. rectangle
+            //      2. ellipse
+            //      3. polygon
+            //      4. continuous polybezier
+            //
+            // The second member is type VDC and contains:
+            //      for rectangle: 4 VDC defining two corner points;
+            //      for ellipse: 6 VDC defining respectively the center, and two CDP endpoints;
+            //      for polygon: 2n VDC defining polygon vertex points
+            //      for polybezier: 2 * (3n + 1) VDC values, representing 3n + 1 points, defining n contiguous cubic bezier segments;
+            //
+            // For polygon and polybezier regions, closure is implicit (if the last given point does not match the first,
+            // then the viewer closes the region with a straight line segment from the last to the first).
+            float regionType = Convert.ToSingle(allValues[0]);
+            switch ((int)regionType)
+            {
+                case 1: // Rectangle
+                    if (allValues.Length == 5)
+                    {
+                        float startX = Convert.ToSingle(allValues[1]);
+                        float startY = Convert.ToSingle(allValues[2]);
+                        float endX = Convert.ToSingle(allValues[3]);
+                        float endY = Convert.ToSingle(allValues[4]);
+                        var start = new PointF(startX, startY);
+                        var end = new PointF(endX, endY);
+
+                        var regionNode = AddNode("@{0} Rectangle ({1} by {2})", applicationStructureAttribute.AttributeType,
+                            Math.Abs(endX - startX), Math.Abs(endY - startY));
+                        regionNode.Nodes.AddRange(new[]
+                        {
+                            new SimpleNode(string.Format("Start: {0}", start)),
+                            new SimpleNode(string.Format("End: {0}", end)),
+                        });
+                    }
+                    else
+                    {
+                        var regionNode = AddNode("@{0} Rectangle (malformed values)", applicationStructureAttribute.AttributeType);
+                        regionNode.Nodes.AddRange(allValues.Skip(1).Select(o => new SimpleNode(Convert.ToString(o))));
+                    }
+                    break;
+                case 2: // Ellipse
+                    if (allValues.Length == 7)
+                    {
+                        float centerX = Convert.ToSingle(allValues[1]);
+                        float centerY = Convert.ToSingle(allValues[2]);
+                        float focus1X = Convert.ToSingle(allValues[3]);
+                        float focus1Y = Convert.ToSingle(allValues[4]);
+                        float focus2X = Convert.ToSingle(allValues[5]);
+                        float focus2Y = Convert.ToSingle(allValues[6]);
+                        var center = new PointF(centerX, centerY);
+                        var focus1 = new PointF(focus1X, focus1Y);
+                        var focus2 = new PointF(focus2X, focus2Y);
+
+                        var regionNode = AddNode("@{0} Ellipse (at {1})", applicationStructureAttribute.AttributeType, center);
+                        regionNode.Nodes.AddRange(new[]
+                        {
+                            new SimpleNode(string.Format("Center: {0}", center)),
+                            new SimpleNode(string.Format("Focal Point 1: {0}", focus1)),
+                            new SimpleNode(string.Format("Focal Point 2: {0}", focus2)),
+                        });
+                    }
+                    else
+                    {
+                        var regionNode = AddNode("@{0} Ellipse (malformed values)", applicationStructureAttribute.AttributeType);
+                        regionNode.Nodes.AddRange(allValues.Skip(1).Select(o => new SimpleNode(Convert.ToString(o))));
+                    }
+                    break;
+                case 3: // Polygon
+                    if (allValues.Length % 2 == 1)
+                    {
+                        var points = allValues.Skip(1)
+                            .Select((o, i) => new { Index = i, Float = Convert.ToSingle(o) })
+                            .GroupBy(a => a.Index / 2)
+                            .Select(g => new PointF(g.First().Float, g.Skip(1).First().Float))
+                            .ToArray();
+                        var regionNode = AddNode("@{0} Polygon ({1} points)", applicationStructureAttribute.AttributeType, points.Length);
+                        regionNode.Nodes.AddRange(points.Select(p => new SimpleNode(p.ToString())));
+                    }
+                    else
+                    {
+                        var regionNode = AddNode("@{0} Rectangle (malformed values)", applicationStructureAttribute.AttributeType);
+                        regionNode.Nodes.AddRange(allValues.Skip(1).Select(o => new SimpleNode(Convert.ToString(o))));
+                    }
+                    break;
+                case 4: // Continuous Polybezier
+                    if (allValues.Length % 6 == 3)
+                    {
+                        var firstPoint = new PointF(Convert.ToSingle(allValues.Skip(1).First()), Convert.ToSingle(allValues.Skip(2).First()));
+                        var pointGroups = allValues.Skip(3)
+                            .Select((o, i) => new { Index = i, Float = Convert.ToSingle(o) })
+                            .GroupBy(a => a.Index / 6)
+                            .Select(g => new[]
+                            {
+                                new PointF(g.First().Float, g.Skip(1).First().Float),
+                                new PointF(g.Skip(2).First().Float, g.Skip(3).First().Float),
+                                new PointF(g.Skip(4).First().Float, g.Skip(5).First().Float),
+                            })
+                            .ToArray();
+                        var points = new List<PointF[]>();
+                        var lastPoint = firstPoint;
+                        foreach (PointF[] pointGroup in pointGroups)
+                        {
+                            points.Add(new[]
+                            {
+                                lastPoint,
+                                pointGroup[0],
+                                pointGroup[1],
+                                pointGroup[2],
+                            });
+                            lastPoint = pointGroup[2];
+                        }
+                        var regionNode = AddNode("@{0} Polybezier ({1} points)", applicationStructureAttribute.AttributeType, points.Count);
+                        regionNode.Nodes.AddRange(points.Select(p => new SimpleNode(string.Format("{0}, {1}, {2}, {3}", p[0], p[1], p[2], p[3]))));
+                    }
+                    else
+                    {
+                        var regionNode = AddNode("@{0} Polybezier (malformed values)", applicationStructureAttribute.AttributeType);
+                        regionNode.Nodes.AddRange(allValues.Skip(1).Select(o => new SimpleNode(Convert.ToString(o))));
+                    }
+                    break;
+                default:
+                    var fallbackNode = AddNode("@{0} Region Type {1} (unsupported)", applicationStructureAttribute.AttributeType, allValues[0]);
+                    fallbackNode.Nodes.AddRange(allValues.Skip(1).Select(o => new SimpleNode(Convert.ToString(o))));
                     break;
             }
         }

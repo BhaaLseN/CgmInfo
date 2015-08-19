d5b9dfb0b4a3f7b2e1e1012dc9c5f0d2fadb9817
BhaaL <bhaal@0x1337.org>
2015-08-19 21:46:50 +0200

    ignore the stored layout if any pane is missing
    ...otherwise it would be missing completely, without the chance to ever see
    it again.

--- CgmInfoGui/MainWindow.xaml.cs
+++ CgmInfoGui/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Windows;
 using System.Xml.Linq;
 using Xceed.Wpf.AvalonDock.Layout.Serialization;
@@ -19,7 +20,8 @@ namespace CgmInfoGui
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             var layout = Properties.Settings.Default.DockLayout;
-            if (layout != null && layout.Root != null)
+            // don't load an existing layout if panels are missing; otherwise they wont be shown
+            if (layout != null && layout.Root != null && !HasMissingPanels(layout))
             {
                 using (var reader = layout.CreateReader())
                 {
@@ -27,6 +29,26 @@ namespace CgmInfoGui
                     serializer.Deserialize(reader);
                 }
             }
+        }
+
+        // this list should always have the same ContentIds listed as the XAML.
+        private static readonly string[] PanelContentIds = { "MetafileStructure", "ApplicationStructure", "XCF" };
+        private static bool HasMissingPanels(XDocument layout)
+        {
+            var rootPanel = layout.Root.Element("RootPanel");
+            if (rootPanel == null)
+                return true;
+            var paneGroup = rootPanel.Element("LayoutDocumentPaneGroup");
+            if (paneGroup == null)
+                return true;
+
+            var existingPanels = paneGroup
+                .Elements("LayoutDocumentPane")
+                .Elements("LayoutDocument")
+                .Attributes("ContentId")
+                .Select(a => a.Value)
+                .ToArray();
+            return PanelContentIds.Except(existingPanels).Any();
         }
 
         protected override void OnClosed(EventArgs e)

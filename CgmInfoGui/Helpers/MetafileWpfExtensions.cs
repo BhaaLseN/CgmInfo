2dca7aa8665b2a609f00322b356424c2ad9208cf
BhaaL <bhaal@0x1337.org>
2015-09-09 19:13:50 +0200

    draw line visuals

--- CgmInfoGui/Helpers/MetafileWpfExtensions.cs
+++ CgmInfoGui/Helpers/MetafileWpfExtensions.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+using System.Windows.Media;
 using CgmInfo.Utilities;
 using WpfPoint = System.Windows.Point;
 
@@ -8,6 +11,24 @@ namespace CgmInfoGui
         public static WpfPoint ToPoint(this MetafilePoint point)
         {
             return new WpfPoint(point.X, point.Y);
+        }
+        public static WpfPoint[] ToPoints(this MetafilePoint[] points)
+        {
+            return points.Select(p => p.ToPoint()).ToArray();
+        }
+
+        public static Color GetColor(this MetafileColor color)
+        {
+            try
+            {
+                var argb = color.ToARGB();
+                return Color.FromArgb((byte)argb.Alpha, (byte)argb.Red, (byte)argb.Green, (byte)argb.Blue);
+            }
+            // not all color spaces are supported yet; make it obvious in case there is one.
+            catch (NotSupportedException)
+            {
+                return Colors.HotPink;
+            }
         }
     }
 }

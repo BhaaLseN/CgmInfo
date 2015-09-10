77501ab25ab30ac1235b0be8d5217c7dc7bb5437
BhaaL <bhaal@0x1337.org>
2015-09-10 20:03:51 +0200

    draw ellipse visuals
    slightly wrong in some cases, but good enough for now

---
+++ CgmInfoGui/Visuals/EllipseVisual.cs
@@ -0,0 +1,37 @@
+using System;
+using Avalonia;
+using Avalonia.Media;
+
+namespace CgmInfoGui.Visuals;
+
+public class EllipseVisual : VisualBase
+{
+    public EllipseVisual(Point center, Point firstConjugateDiameter, Point secondConjugateDiameter)
+    {
+        Center = center;
+        FirstConjugateDiameter = firstConjugateDiameter;
+        SecondConjugateDiameter = secondConjugateDiameter;
+
+        // calculate the actual radius in X/Y direction
+        RadiusX = Distance(Center, FirstConjugateDiameter);
+        RadiusY = Distance(Center, SecondConjugateDiameter);
+        // calculate angle between first CDP and center
+        Rad = Math.Atan2(FirstConjugateDiameter.Y - Center.Y, FirstConjugateDiameter.X - Center.X);
+        Angle = RadiansToDegrees(Rad);
+    }
+    public Point Center { get; }
+    public Point FirstConjugateDiameter { get; }
+    public Point SecondConjugateDiameter { get; }
+
+    public double RadiusX { get; }
+    public double RadiusY { get; }
+    public double Rad { get; }
+    public double Angle { get; }
+
+    protected internal override void DrawTo(DrawingContext drawingContext)
+    {
+        // FIXME: use EDGE* and INTERIOR* here.
+        using (drawingContext.PushTransform(Matrix.CreateTranslation(Center.X, Center.Y) * Matrix.CreateRotation(Rad)))
+            drawingContext.DrawEllipse(null, GetBlack(), Center, RadiusX, RadiusY);
+    }
+}

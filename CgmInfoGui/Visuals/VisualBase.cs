cda170a466f3f07df114eec9eb2049b23c7888ea
BhaaL <bhaal@0x1337.org>
2015-09-10 20:03:51 +0200

    draw ellipse visuals
    slightly wrong in some cases, but good enough for now

--- CgmInfoGui/Visuals/VisualBase.cs
+++ CgmInfoGui/Visuals/VisualBase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Avalonia;
 using Avalonia.Media;
 
 namespace CgmInfoGui.Visuals;
@@ -20,4 +21,13 @@ public abstract class VisualBase
 
     private static readonly Lazy<Color> BackgroundColor = new(() => /*App.Current?.ActualThemeVariant == Avalonia.Styling.ThemeVariant.Dark ? Colors.Black :*/ Colors.LightGray);
     protected internal static Color GetBackgroundColor() => BackgroundColor.Value;
+
+    public static double RadiansToDegrees(double rad)
+    {
+        return 180 / Math.PI * rad;
+    }
+    public static double Distance(Point p1, Point p2)
+    {
+        return Math.Sqrt(Math.Pow(p2.X - p1.X, 2.0) + Math.Pow(p2.Y - p1.Y, 2.0));
+    }
 }

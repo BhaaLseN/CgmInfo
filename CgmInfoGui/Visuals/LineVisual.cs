831827f93e119524de1296d5c7b5e0025bd2d573
BhaaL <bhaal@0x1337.org>
2015-09-13 15:25:14 +0200

    draw polygon visuals

--- CgmInfoGui/Visuals/LineVisual.cs
+++ CgmInfoGui/Visuals/LineVisual.cs
@@ -7,12 +7,18 @@ namespace CgmInfoGui.Visuals
     public class LineVisual : VisualBase
     {
         public LineVisual(Point[] points, Pen pen)
+            : this(points, pen, false)
         {
+        }
+        public LineVisual(Point[] points, Pen pen, bool isClosed)
+        {
+            IsClosed = isClosed;
             Points = points;
             Pen = pen;
         }
         public Point[] Points { get; private set; }
         public Pen Pen { get; private set; }
+        public bool IsClosed { get; private set; }
 
         protected internal override void DrawTo(DrawingContext drawingContext, VisualContext visualContext)
         {
@@ -20,7 +26,7 @@ namespace CgmInfoGui.Visuals
             using (var ctx = geo.Open())
             {
                 Point[] points = Points.Select(p => visualContext.Correct(p)).ToArray();
-                ctx.BeginFigure(points[0], false, false);
+                ctx.BeginFigure(points[0], false, IsClosed);
                 ctx.PolyLineTo(points.Skip(1).ToList(), true, false);
             }
             geo.Freeze();

c5a90afce153f2cef0c52904ae1b8682713428eb
BhaaL <bhaal@0x1337.org>
2017-05-12 15:32:00 +0200

    apply text visual properties where we can

--- CgmInfoGui/Visuals/TextVisual.cs
+++ CgmInfoGui/Visuals/TextVisual.cs
@@ -6,17 +6,29 @@ namespace CgmInfoGui.Visuals;
 
 public class TextVisual : VisualBase
 {
-    public TextVisual(string text, Point location)
+    public TextVisual(string text, Point location, FontFamily fontFamily, double fontSize, Color fontColor)
     {
         Text = text;
         Location = location;
+        Font = fontFamily;
+        FontSize = fontSize;
+        FontColor = fontColor;
     }
+
     public string Text { get; set; }
-    public Point Location { get; }
+    public Point Location { get; internal set; }
+    public double FontSize { get; }
+    public Color FontColor { get; }
+    public FontFamily Font { get; }
 
     protected internal override void DrawTo(DrawingContext drawingContext, VisualContext visualContext)
     {
-        var formattedText = new FormattedText(Text, CultureInfo.GetCultureInfo("en"), FlowDirection.LeftToRight, new Typeface(new FontFamily("Arial"), FontStyle.Normal, FontWeight.Normal, FontStretch.Normal), 12, Brushes.Black);
+        var formattedText = CreateFormattedText(new SolidColorBrush(FontColor));
         drawingContext.DrawText(formattedText, visualContext.Correct(Location));
+    }
+
+    internal FormattedText CreateFormattedText(IBrush? textColor)
+    {
+        return new FormattedText(Text, CultureInfo.GetCultureInfo("en"), FlowDirection.LeftToRight, new Typeface(Font, FontStyle.Normal, FontWeight.Normal, FontStretch.Normal), FontSize, textColor);
     }
 }

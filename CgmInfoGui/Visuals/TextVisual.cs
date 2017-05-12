7442a2bd5b59cd7e1d76a610bca9e1c100c0c926
BhaaL <bhaal@0x1337.org>
2017-05-12 15:32:00 +0200

    apply text visual properties where we can

--- CgmInfoGui/Visuals/TextVisual.cs
+++ CgmInfoGui/Visuals/TextVisual.cs
@@ -6,20 +6,32 @@ namespace CgmInfoGui.Visuals
 {
     public class TextVisual : VisualBase
     {
-        public TextVisual(string text, Point location)
+        public TextVisual(string text, Point location, FontFamily fontFamily, double fontSize, Brush fontBrush)
         {
             Text = text;
             Location = location;
+            Font = fontFamily;
+            FontSize = fontSize;
+            FontBrush = fontBrush;
         }
+
         public string Text { get; set; }
-        public Point Location { get; private set; }
+        public Point Location { get; internal set; }
+        public double FontSize { get; }
+        public Brush FontBrush { get; }
+        public FontFamily Font { get; }
 
         internal static double PixelsPerDip { get; set; } = 1.0d;
 
         protected internal override void DrawTo(DrawingContext drawingContext, VisualContext visualContext)
         {
-            var formattedText = new FormattedText(Text, CultureInfo.GetCultureInfo("en"), FlowDirection.LeftToRight, new Typeface(new FontFamily("Arial"), new FontStyle(), new FontWeight(), new FontStretch()), 12, Brushes.Black, PixelsPerDip);
+            var formattedText = CreateFormattedText();
             drawingContext.DrawText(formattedText, visualContext.Correct(Location));
+        }
+
+        internal FormattedText CreateFormattedText()
+        {
+            return new FormattedText(Text, CultureInfo.GetCultureInfo("en"), FlowDirection.LeftToRight, new Typeface(Font, new FontStyle(), new FontWeight(), new FontStretch()), FontSize, FontBrush, PixelsPerDip);
         }
     }
 }

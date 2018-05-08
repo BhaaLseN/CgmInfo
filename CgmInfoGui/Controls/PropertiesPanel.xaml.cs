a1f0de33dea0cb071e68a9ff233264089c2339c5
BhaaL <bhaal@0x1337.org>
2018-05-08 20:59:10 +0200

    structure: show the selected node in a property grid
    they usually have more data, even though we may not show it yet.

---
+++ CgmInfoGui/Controls/PropertiesPanel.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CgmInfoGui.Controls
+{
+    /// <summary>
+    /// Interaction logic for PropertiesPanel.xaml
+    /// </summary>
+    public partial class PropertiesPanel : UserControl
+    {
+        public PropertiesPanel()
+        {
+            InitializeComponent();
+        }
+
+        public object Source
+        {
+            get { return (object)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register(nameof(Source), typeof(object), typeof(PropertiesPanel), new PropertyMetadata(OnSourceChanged));
+
+        private static void OnSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((PropertiesPanel)d).OnSourceChanged(e);
+        }
+        private void OnSourceChanged(DependencyPropertyChangedEventArgs e)
+        {
+            // TODO: update data template to show either property grid or raw view
+        }
+    }
+}

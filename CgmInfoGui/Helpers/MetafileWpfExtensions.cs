7c64a5beefa2832f66fba8edd217847556692832
BhaaL <bhaalsen@gmail.com>
2025-02-16 18:54:28 +0100

    draw line visuals

--- CgmInfoGui/Helpers/MetafileWpfExtensions.cs
+++ CgmInfoGui/Helpers/MetafileWpfExtensions.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Avalonia.Media;
 using CgmInfo.Utilities;
 using AvaloniaPoint = Avalonia.Point;
 
@@ -8,5 +12,29 @@ public static class MetafileWpfExtensions
     public static AvaloniaPoint ToPoint(this MetafilePoint point)
     {
         return new AvaloniaPoint(point.X, point.Y);
+    }
+    public static AvaloniaPoint[] ToPoints(this MetafilePoint[] points)
+    {
+        return points.Select(p => p.ToPoint()).ToArray();
+    }
+
+    public static Color GetColor(this MetafileColor color)
+    {
+        try
+        {
+            var argb = color.ToARGB();
+            return Color.FromArgb((byte)argb.Alpha, (byte)argb.Red, (byte)argb.Green, (byte)argb.Blue);
+        }
+        // not all color spaces are supported yet; make it obvious in case there is one.
+        catch (NotSupportedException)
+        {
+            return Colors.HotPink;
+        }
+    }
+
+    public static void PolyLineTo(this StreamGeometryContext context, IEnumerable<AvaloniaPoint> points, bool isStroked)
+    {
+        foreach (var point in points)
+            context.LineTo(point, isStroked);
     }
 }
--- CgmInfoGui/Traversal/GraphicalElementBuilderVisitor.cs
+++ CgmInfoGui/Traversal/GraphicalElementBuilderVisitor.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using Avalonia;
+using CgmInfo.Commands.Attributes;
 using CgmInfo.Commands.Enums;
 using CgmInfo.Commands.GraphicalPrimitives;
 using CgmInfo.Commands.MetafileDescriptor;
@@ -13,7 +15,39 @@ public class GraphicalElementBuilderVisitor : CommandVisitor<GraphicalElementCon
     {
         parameter.Visuals.VdcExtent = new Rect(maximumVdcExtent.FirstCorner.ToPoint(), maximumVdcExtent.SecondCorner.ToPoint());
     }
+    public override void AcceptAttributeLineColor(LineColor lineColor, GraphicalElementContext parameter)
+    {
+        parameter.LineAttributes.LineColor = lineColor.Color.GetColor();
+    }
+    public override void AcceptAttributeLineType(LineType lineType, GraphicalElementContext parameter)
+    {
+        parameter.LineAttributes.LineType = lineType.Index;
+    }
+    public override void AcceptAttributeLineWidth(LineWidth lineWidth, GraphicalElementContext parameter)
+    {
+        parameter.LineAttributes.LineWidth = lineWidth.Width;
+    }
+    public override void AcceptAttributeLineCap(LineCap lineCap, GraphicalElementContext parameter)
+    {
+        parameter.LineAttributes.DashCap = lineCap.DashCapIndicator;
+        parameter.LineAttributes.LineCap = lineCap.LineCapIndicator;
+    }
+    public override void AcceptAttributeLineJoin(LineJoin lineJoin, GraphicalElementContext parameter)
+    {
+        parameter.LineAttributes.LineJoin = lineJoin.Index;
+    }
+    public override void AcceptControlMiterLimit(MiterLimit miterLimit, GraphicalElementContext parameter)
+    {
+        parameter.LineAttributes.MiterLimit = miterLimit.Limit;
+    }
 
+    public override void AcceptGraphicalPrimitivePolyline(Polyline polyline, GraphicalElementContext parameter)
+    {
+        var line = new LineVisual(polyline.Points.ToPoints(), parameter.LineAttributes.GetPen);
+        foreach (var point in polyline.Points.Select(p => p.ToPoint()))
+            parameter.IncreaseBounds(point);
+        parameter.Add(line);
+    }
     public override void AcceptGraphicalPrimitiveText(TextCommand text, GraphicalElementContext parameter)
     {
         var textVisual = new TextVisual(text.Text, text.Position.ToPoint());

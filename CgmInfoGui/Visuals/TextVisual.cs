bf1c075c3f43d94ea3d976b6c15ff72c5efa1232
BhaaL <bhaalsen@gmail.com>
2025-03-07 17:06:55 +0100

    introduce VisualContext to correct for VDC coodinates
    coordinates need not be the same direction as WPF coordinates, which are
    positive X = +1.0 (right) and positive Y = -1.0 (down)

--- CgmInfoGui/Visuals/TextVisual.cs
+++ CgmInfoGui/Visuals/TextVisual.cs
@@ -14,9 +14,9 @@ public class TextVisual : VisualBase
     public string Text { get; set; }
     public Point Location { get; }
 
-    protected internal override void DrawTo(DrawingContext drawingContext)
+    protected internal override void DrawTo(DrawingContext drawingContext, VisualContext visualContext)
     {
         var formattedText = new FormattedText(Text, CultureInfo.GetCultureInfo("en"), FlowDirection.LeftToRight, new Typeface(new FontFamily("Arial"), FontStyle.Normal, FontWeight.Normal, FontStretch.Normal), 12, Brushes.Black);
-        drawingContext.DrawText(formattedText, Location);
+        drawingContext.DrawText(formattedText, visualContext.Correct(Location));
     }
 }
